dacc7d40d64d3f74aea781bbb9a2210abee703b4
benpptung <benpptung@tacol.biz>
2023-01-02 14:02:25 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
 'use strict'
 
 const Err = require('../lib/index').Err
+const expect = require('expect.js')
 
-var res
-var need = 'blah'
+describe('Err()', function() {
 
-var er = new Err('no response', {res, need})
+  it('return a wrapped Error object', function() {
 
-console.log((er instanceof Error)) // true
+    var res
+    var need = 'blah'
+    var act = 3
+    var err = Err('no message', {res, need}, act)
+
+    expect(err).to.be.a(Error)
+    expect(err.message).to.be('no message')
+    expect(err.messages[0]).to.be('no message')
+    expect(err.messages.length).to.be(1)
+    expect(err.original.res).to.be(res)
+    expect(err.original.need).to.be(need)
+    expect(err.actions.length).to.be(1)
+    expect(err.actions[0]).to.be(act)
+  })
+})
